14fc1edba2a88f406df97623794a72aefed244a8
chrisxkeith <chris.keith@gmail.com>
2017-11-24 10:27:14 -0800

    Compiles with elecrow library. Still doesn't return expected values.

--- blink-with-humidity/blink-with-humidity.ino
+++ blink-with-humidity/blink-with-humidity.ino
@@ -1,8 +1,13 @@
-/*
+ /*
   Test humidity sensors.
   A1 : https://www.amazon.com/Digital-Temperature-Humidity-measure-Arduino/dp/B00R4KGG6Q
   A2 : https://www.amazon.com/Digital-Temperature-Humidity-measure-Arduino/dp/B018JO5BRK
+  https://www.elecrow.com/wiki/index.php?title=Temperature_%26_Humidity_Sensor
+  - library (.h/.cpp) in ~/Documents/Arduino/libraries/Humidity_Temperature_Sensor
+  - ino in ~/Documents/Github/arduino-humidity-sensor/blink-with-humidity
 */
+
+/*
 long masterCount = 0;
 long HT21Count = 0;
 long HT21Average = 0;
@@ -120,4 +125,46 @@ void loop() {
   }
   delay(1);
   masterCount++;
+}
+*/
+#include "DHT.h"
+
+#define DHTPIN 2     // what pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11 
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("DHTxx test!");
+ 
+  dht.begin();
+}
+
+void loop() {
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // check if returns are valid, if they are NaN (not a number) then something went wrong!
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+  } else {
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }
 }
